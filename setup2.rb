a77db1d4db987759d94a01c1ac60c5b29e050f44
Andrew Breyen <andrew.breyen@gmail.com>
2023-07-30 01:28:04 -0500

    Update setup2.rb

--- setup2.rb
+++ setup2.rb
@@ -1,6 +1,7 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
+require 'json' # Require the JSON module for parsing JSON data
 
 options = {}
 OptionParser.new do |opts|
@@ -10,8 +11,8 @@ OptionParser.new do |opts|
     options[:force] = force
   end
 
-  opts.on('-n', '--no-update', "Skip updating Homebrew to the latest version") do |no_update|
-    options[:no_update] = no_update
+  opts.on('-n', '--no-update', "Skip updating Homebrew to the latest version") do
+    options[:no_update] = true
   end
 
   opts.on('-h', '--help', "Print all the available arguments") do
@@ -42,18 +43,43 @@ def update_homebrew
   system('brew update')
 end
 
+# Function to fetch the latest minor version of a package from Homebrew
+def latest_minor_version(package_name)
+  latest_version = `brew info --json=v1 #{package_name}`.lines.map do |line|
+    JSON.parse(line)["versions"]
+  end.flatten.select do |version|
+    version.match?(/^\d+\.\d+\.\d+$/)
+  end.max_by do |version|
+    Gem::Version.new(version)
+  end
+
+  latest_version
+end
+
 # Function to install a package using Homebrew
-def install_package(package_name)
-  puts "Installing #{package_name}..."
-  system("brew install #{package_name}")
+def install_package(package_name, version = nil)
+  if version.nil?
+    version = latest_minor_version(package_name)
+  end
+
+  if version
+    puts "Installing #{package_name}@#{version}..."
+    system("brew install #{package_name}@#{version}")
+  else
+    puts "No suitable version found for #{package_name}."
+  end
 end
 
 # Runner method to execute all the commands
 def run_setup(options)
-  # Large banner indicating the script is starting
-  puts "========================================================================"
-  puts "                  Welcome to the Setup Script                            "
-  puts "========================================================================"
+  # Custom welcome banner
+  puts <<~BANNER
+    #####
+    #####
+    ##### Mac Setup
+    #####
+
+  BANNER
 
   # Check if Homebrew is not installed or if the --force flag is provided
   unless homebrew_installed? || options[:force]
@@ -64,23 +90,26 @@ def run_setup(options)
   end
 
   # Update Homebrew to the latest version unless --no-update is specified
-  unless options[:no_update]
+  unless options.key?(:no_update) && options[:no_update]
     update_homebrew
   else
     puts "Skipping update..."
   end
 
   # Install packages
-  install_package('pyenv')
+  install_package('pyenv', '2.3.23')
   install_package('gh')
   install_package('coreutils')
-  
-  
 
-  # Completion message indicating the script has finished
-  puts "========================================================================"
-  puts "                Setup Completed Successfully!                           "
-  puts "========================================================================"
+  # Custom completion message
+ puts <<~BANNER
+    #####
+    #####
+    ##### Mac Setup Completed successfully
+    #####
+    
+  BANNER
+
 end
 
 # Call the runner method to execute all the commands
