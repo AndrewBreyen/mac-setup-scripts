8011bba3da45e5b8687fb1be86f7b9ecf55059fc
Andrew Breyen <andrew.breyen@gmail.com>
2023-07-30 01:50:57 -0500

    install using `install_package('pyenv')`

--- setup2.rb
+++ setup2.rb
@@ -1,27 +1,29 @@
 #!/usr/bin/env ruby
 
 require 'optparse'
-require 'json' # Require the JSON module for parsing JSON data
+require 'json'
+require 'pry'
+
 
 options = {}
 OptionParser.new do |opts|
   opts.banner = "Usage: setup.rb [options]"
-
-  opts.on('-f', '--force', "Force Homebrew to install, even if it is already installed") do |force|
-    options[:force] = force
-  end
-
-  opts.on('-n', '--no-update', "Skip updating Homebrew to the latest version") do
-    options[:no_update] = true
-  end
 
   opts.on('-h', '--help', "Print all the available arguments") do
     puts opts
     exit
   end
 
-  opts.on('-d', '--dry-run', "Run the script as dry-run. No changes will be made.") do |force|
-    options[:force] = force
+  opts.on('-f', '--force', "Force Homebrew to install, even if it is already installed") do 
+    options[:force] = true
+  end
+
+  opts.on('-n', '--no-update', "Skip updating Homebrew to the latest version") do
+    options[:no_update] = true
+  end
+
+  opts.on('-d', '--dry-run', "Run the script as dry-run. No changes will be made.") do
+    options[:dry_run] = true
   end
 
 end.parse!
@@ -34,6 +36,7 @@ end
 # Function to install Homebrew
 def install_homebrew
   puts "Installing Homebrew..."
+  system('export HOMEBREW_NO_INSTALL_FROM_API=1')
   system('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
 end
 
@@ -43,31 +46,38 @@ def update_homebrew
   system('brew update')
 end
 
-# Function to fetch the latest minor version of a package from Homebrew
-def latest_minor_version(package_name)
-  latest_version = `brew info --json=v1 #{package_name}`.lines.map do |line|
-    JSON.parse(line)["versions"]
-  end.flatten.select do |version|
-    version.match?(/^\d+\.\d+\.\d+$/)
-  end.max_by do |version|
-    Gem::Version.new(version)
-  end
+# # Function to fetch the latest minor version of a package from Homebrew
+# def latest_minor_version(package_name)
+#   latest_version = `brew list --versions #{package_name}`.lines.map do |line|
+#     line.match(/#{package_name}@(\d+\.\d+\.\d+)/)&.captures&.first
+#   end.compact.select do |version|
+#     version.match?(/^\d+\.\d+\.\d+$/)
+#   end.max_by do |version|
+#     Gem::Version.new(version)
+#   end
 
-  latest_version
-end
+#   latest_version
+# end
 
-# Function to install a package using Homebrew
-def install_package(package_name, version = nil)
-  if version.nil?
-    version = latest_minor_version(package_name)
-  end
+# # Function to install a package using Homebrew
+# def install_package(package_name, version = nil)
+#   # binding.pry
+#   if version.nil?
+#     version = latest_minor_version(package_name)
+#   end
 
-  if version
-    puts "Installing #{package_name}@#{version}..."
-    system("brew install #{package_name}@#{version}")
-  else
-    puts "No suitable version found for #{package_name}."
-  end
+#   if version
+#     puts "Installing #{package_name}@#{version}..."
+#     system("brew install #{package_name}@#{version}")
+#   else
+#     puts "No suitable version found for #{package_name}."
+#   end
+# end
+
+
+def install_package(package_name)
+  puts "Installing #{package_name}..."
+  system("brew install #{package_name}")
 end
 
 # Runner method to execute all the commands
@@ -81,6 +91,7 @@ def run_setup(options)
 
   BANNER
 
+  
   # Check if Homebrew is not installed or if the --force flag is provided
   unless homebrew_installed? || options[:force]
     install_homebrew
@@ -97,7 +108,7 @@ def run_setup(options)
   end
 
   # Install packages
-  install_package('pyenv', '2.3.23')
+  install_package('pyenv')
   install_package('gh')
   install_package('coreutils')
 
